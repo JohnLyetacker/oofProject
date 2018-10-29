450c1326c22f0710c8ee4f3d5c2309b2a71d4849
JohnLyetacker <44590586+JohnLyetacker@users.noreply.github.com>
2018-10-30 02:43:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 "use strict";
 var profession;
 profession=prompt('Какова твоя профессия?', 'Повар');
-if(profession='Повар'){
-	alert('Выберите что-то иное')
+
+if (profession=='Повар') {
+	alert('Выберите что-то иное');
+} else if (profession=='Кодзима') {
+	alert('Гений');
+} else {
+	alert('Ты самый заурядный дебич');
 }
